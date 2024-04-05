58795d71e0409bec2dccfc3ec01640aec897dcf1
Tony Manuel <tony.manuel@zensar.com>
2024-04-06 01:19:28 +0530

    Menuoptions

--- src/components/ClausesAndOptions/ClausesAndOptions.jsx
+++ src/components/ClausesAndOptions/ClausesAndOptions.jsx
@@ -5,24 +5,20 @@ export const ClausesAndOptions = ({ options }) => {
 
     const inputRef = useRef([]);
 
-
-
-    const change = (idx, type) => {
+    const change = (idx) => {
         let item = inputRef.current[idx]
         item.classList.add("active");
-
-
     }
 
 
     return <div className="template-option-groups">
         <div style={{ width: "100%" }}>
             <div className="template-options level-1">
-                <p ref={el => inputRef.current[0] = el} onClick={() => change(0, "main")}>Core Clause</p>
+                <p ref={el => inputRef.current[0] = el} onClick={() => change(0)}>Core Clause</p>
                 <div className="template-options level-2">
-                    <p ref={el => inputRef.current[1] = el} onClick={() => change(1, "secondary")}>General</p>
+                    <p ref={el => inputRef.current[1] = el} onClick={() => change(1)}>General</p>
                     <div className="template-options level-3">
-                        <p ref={el => inputRef.current[2] = el} onClick={() => change(2, "tertiary")}>Actionsd</p>
+                        <p ref={el => inputRef.current[2] = el} onClick={() => change(2)}>Actionsd</p>
                         <p>Actions</p>
                         <p>Actions</p>
                         <p>Actions</p>
@@ -32,32 +28,19 @@ export const ClausesAndOptions = ({ options }) => {
             </div>
             <div className="divider" />
         </div>
-        <div style={{ width: "100%" }}>
-            <div className="template-options level-1">
-                <p ref={el => inputRef.current[0] = el} onClick={() => change(0, "main")}>Core Clause</p>
-                <div className="template-options level-2">
-                    <p ref={el => inputRef.current[1] = el} onClick={() => change(1, "secondary")}>General</p>
-                    <div className="template-options level-3">
-                        <p ref={el => inputRef.current[2] = el} onClick={() => change(2, "tertiary")}>Actionsd</p>
-                        <p>Actions</p>
-                        <p>Actions</p>
-                        <p>Actions</p>
-                    </div>
-                </div>
 
-            </div>
-            {/* <div className="divider" /> */}
-        </div>
-
-
-
-
-
-
-
-
-
-
+        {/* <div className="divider" /> */}
     </div>
+
+
+
+
+
+
+
+
+
+
+
 
 }
