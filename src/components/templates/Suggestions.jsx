464725631f3f6528420ddb506d8b25326f103b1f
Tony Manuel <tony.manuel@zensar.com>
2024-04-08 01:17:08 +0530

    Suggestion Item

--- src/components/templates/Suggestions.jsx
+++ src/components/templates/Suggestions.jsx
@@ -1,34 +1,56 @@
+import { useState } from "react"
 import { AddIcon, StarIcon, TrashIcon } from "../../assets/IconList"
 import { ButtonSmall } from "../button/Button"
 import { Icon } from "../icon/Icon"
+
+
+
+
+
 
 export const Suggestions = () => {
     return <div className="suggestions">
         <div className="suggestions-title">
             <Icon component={<StarIcon />} variant="null" />
             <p>AI Suggestions</p>
+
         </div>
-
-        <div className="suggestion-details">
-            <label className="suggestion-details-title">Missing clause title</label>
-            <p className="suggestion-details-desc">This is an example description of the clauses suggested by AI that user missed in the contract. Upon clicking on accept this clause will be added into the contract, clicking on delete icon will remove this suggestion from the list of suggestions.</p>
-            <div className="suggestion-actions">
-                <ButtonSmall icon={<AddIcon />} label="Add Clause" />
-                <Icon component={<TrashIcon />} />
-
-            </div>
+        <div className="suggestion-list">
+            <SuggestionItem />
+            <SuggestionItem />
+            <SuggestionItem />
+            <SuggestionItem />
         </div>
-        <div>
-            <div className="suggestion-list">
-                <p>Missing clause title</p>
-            </div>
-
-            <div className="suggestion-list">
-                <p>Missing clause title</p>
-            </div>
-        </div>
-
 
 
     </div>
+
+
+
+
+}
+
+const SuggestionItem = ({ title = "Missing clause title", desc = "This is an example description of the clauses suggested by AI that user missed in the contract. Upon clicking on accept this clause will be added into the contract, clicking on delete icon will remove this suggestion from the list of suggestions." }) => {
+    const [showDetails, showDetailsHandler] = useState(false)
+
+    return <div>
+        {!showDetails ?
+            <div className="suggestion-item" onClick={() => { showDetailsHandler(true) }}>
+                <p>Missing clause title</p>
+            </div> :
+            <div className="suggestion-details">
+                <label className="suggestion-details-title">{title}</label>
+                <p className="suggestion-details-desc">{desc}</p>
+                <div className="suggestion-actions">
+                    <ButtonSmall icon={<AddIcon />} label="Add Clause" />
+                    <Icon component={<TrashIcon />} onClick={() => showDetailsHandler(false)} />
+
+                </div>
+            </div>
+        }
+    </div>
+
+
+
+
 }
